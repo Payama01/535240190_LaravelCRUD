1df15878086020aa33af794c7532ae2e65f23298
Payama01 <muhammaderki@gmail.com>
2025-05-19 00:07:57 +0700

    10.4.1

--- app/Models/create_mahasiswa_model.php
+++ app/Models/create_mahasiswa_model.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class create_mahasiswa_model extends Model
 {
-    //
+    //add name and email to fillable
+    protected $fillable = ['name','email'];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\homeController;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('home');
+// });
+
+Route::get('/',[homeController::class, 'index']);
+
+Route::get('/about',function () {
+    echo "Halaman About";
+});
+
+Route::get('/mahasiswa',function () {
+    echo "Halaman Mahasiswa";
+});
+
+Route::get('/user/{id}',function ($id) {
+    echo 'echo ID: ' . $id;
 });
